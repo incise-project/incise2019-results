6521103721d95872e5a2d973eee2de4e98231124
mattkerlogue <matt.kerlogue@me.com>
2024-01-10 12:11:11 +0000

    Make results chapter interactive

--- R/country_visuals.R
+++ R/country_visuals.R
@@ -140,10 +140,97 @@ readr::write_excel_csv(radar_grid, "country_visuals/radar_grid.csv")
 # bar data ----------------------------------------------------------------
 
 bar_df <- incise2019_long |>
-  dplyr::filter(indicator == "incise_index") |>
+  dplyr::mutate(
+    rank_value = rank(-value, ties.method = "min"),
+    .by = "indicator"
+  ) |>
   dplyr::mutate(
     value_label = scales::comma(value, accuracy = 0.01),
-    hover_label = paste0(cc_iso3c, ": ", value_label)
+    hover_label = paste0(cc_iso3c, ": ", value_label),
+    colour_group = dplyr::case_when(
+      cc_iso3c == "InCiSE" & indicator == "incise_index" ~ "#009d94",
+      cc_iso3c == "InCiSE" ~ "#999999",
+      indicator == "incise_index" ~ "#dddddd",
+      rank_value <= 5 ~ "#009d94",
+      TRUE ~ "#dddddd"
+    ),
+    text_colour = dplyr::case_when(
+      cc_iso3c == "InCiSE" & indicator == "incise_index" ~ "#009d94",
+      cc_iso3c == "InCiSE" ~ "#444444",
+      indicator == "incise_index" ~ "#444444",
+      rank_value <= 5 ~ "#009d94",
+      TRUE ~ "#444444"
+    ),
+    text_weight = dplyr::case_when(
+      cc_iso3c == "InCiSE"  | rank_value <= 5 & indicator != "incise_index" ~
+        "bold",
+      TRUE ~ "normal"
+    )
   )
 
-readr::write_excel_csv(bar_df, "country_visuals/bar_data.csv")
+index_bar <- bar_df |>
+  dplyr::filter(indicator == "incise_index") |>
+  dplyr::select(cc_iso3c, value, value_label, hover_label, text_weight, text_colour, colour_group)
+
+readr::write_excel_csv(bar_df, "country_visuals/bar_data_full.csv")
+readr::write_excel_csv(index_bar, "country_visuals/bar_data_index.csv")
+
+# incise radar ------------------------------------------------------------
+
+incise_summary <- incise2019_long |>
+  dplyr::summarise(
+    lower = quantile(value, 0.25),
+    upper = quantile(value, 0.75),
+    .by = "indicator"
+  ) |>
+  tidyr::pivot_longer(
+    cols = -indicator,
+    names_to = "cc_iso3c",
+    values_to = "value"
+  )
+
+incise_radar <- incise2019_long |>
+  dplyr::filter(cc_iso3c == "InCiSE") |>
+  dplyr::bind_rows(incise_summary) |>
+  dplyr::filter(indicator != "incise_index") |>
+  dplyr::mutate(
+    indicator = dplyr::case_match(
+      indicator,
+      "ind_cap" ~ "Capabilities",
+      "ind_crm" ~ "Crisis and risk management",
+      "ind_dig" ~ "Digital services",
+      "ind_ffm" ~ "Fiscal and financial management",
+      "ind_hrm" ~ "HR management",
+      "ind_inc" ~ "Inclusiveness",
+      "ind_int" ~ "Integrity",
+      "ind_opn" ~ "Openness",
+      "ind_pol" ~ "Policy making",
+      "ind_pro" ~ "Procurement",
+      "ind_reg" ~ "Regulation",
+      "ind_tax" ~ "Tax administration"
+    ),
+    indicator = stringr::str_wrap(indicator, 13),
+    indicator = forcats::as_factor(indicator),
+    colour_group = dplyr::case_match(
+      cc_iso3c,
+      "upper" ~ "#999999",
+      "lower" ~ "#999999",
+      "InCISE" ~ "#009d94"
+    ),
+    cc_iso3c = dplyr::case_match(
+      cc_iso3c,
+      "upper" ~ "Upper quartile",
+      "lower" ~ "Lower quartile",
+      "InCiSE" ~ "InCiSE average"
+    ),
+    theta = as.numeric(indicator),
+    theta = (360*(theta/max(theta))-105) * pi / 180,
+    new_x = cos(theta) * value,
+    new_y = sin(theta) * -value,
+    data_label = paste0(
+      cc_iso3c, " - ", indicator, ": ", scales::comma(value, accuracy = 0.01)
+    ),
+    data_label = stringr::str_wrap(data_label, 20)
+  )
+
+readr::write_csv(incise_radar, "country_visuals/radar_incise.csv")
