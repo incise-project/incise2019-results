389cbf9320594a37df4d4d8ef16f06e3cb4c5917
Matt Kerlogue <matt.kerlogue@me.com>
2024-01-31 01:02:48 +0000

    Only check narratives locally

--- prepare.R
+++ prepare.R
@@ -1,18 +1,22 @@
 # preparation script
 
-# check country narratives
-old_snap <- readRDS("country_narratives/.fs_snap")
-nf <- fs::dir_ls("country_narratives/")
-new_snap <- tools::md5sum(nf)
+if (interactive()) {
+  # check country narratives
+  old_snap <- readRDS("country_narratives/.fs_snap")
+  nf <- fs::dir_ls("country_narratives/")
+  new_snap <- tools::md5sum(nf)
 
-# re-generate country quarto files
-if (length(old_snap) > length(new_snap)) {
-  stop("Files have been deleted from `country_narratives`")
-} else if (length(old_snap) < length(new_snap)) {
-  stop("Files have been added to `country_narratives`")
-} else if (!identical(old_snap, new_snap)) {
-  message("Narratives have changed, generating new country files")
-  #source("R/generate_country_qmd.R")
+  # re-generate country quarto files
+  if (length(old_snap) > length(new_snap)) {
+    stop("Files have been deleted from `country_narratives`")
+  } else if (length(old_snap) < length(new_snap)) {
+    stop("Files have been added to `country_narratives`")
+  } else if (!identical(old_snap, new_snap)) {
+    message("Narratives have changed, generating new country files")
+    source("R/generate_country_qmd.R")
+  } else {
+    message("Country narratives unchanged")
+  }
 } else {
-  message("Country narratives unchanged")
+  message("Non-interactive session, narratives not checked")
 }
